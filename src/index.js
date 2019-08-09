9f214ccd5884554cb1652925a850e98191010674
Albert Brand <abrand@xebia.com>
2019-08-09 15:48:20 +0200

    Expose abort & retry function, watch value wrapped arguments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { value, onCreated, onBeforeDestroy } from "vue-function-api";
+import { value, watch, onBeforeDestroy } from "vue-function-api";
+
+function isWrapped(obj) {
+  return obj !== undefined && obj !== null && obj._internal instanceof Object;
+}
 
 /**
  * Async helper function that returns three reactive values:
@@ -6,21 +10,52 @@ import { value, onCreated, onBeforeDestroy } from "vue-function-api";
  * * `data`, contains the resolved value in the fulfilled state; and
  * * `error`, contains the exception in the rejected state.
  *
- * @param {function} promiseFn A function that returns a Promise.
- * @param {object} params Parameters passed as first argument to the promise function.
- * @returns {object} Object literal containing `isLoading`, `error` and `data` value wrappers.
+ * It returns the following functions as well:
+ * * `abort`, that aborts the current promise
+ * * `retry`, that retries the original promise
+ *
+ * @param {function|ValueWrapper} promiseFn (optionally wrapped) function that returns a Promise.
+ * @param {object|ValueWrapper} params (optionally wrapped) parameters passed as first argument to the promise function.
+ * @returns {object} Object literal containing `isLoading`, `error` and `data` value wrappers and `abort` and `retry`
+ * functions.
  */
 export function useAsync(promiseFn, params) {
-  const isLoading = value(true);
-  const error = value(null);
-  const data = value(null);
+  // always wrap arguments
+  const wrapPromiseFn = isWrapped(promiseFn) ? promiseFn : value(promiseFn);
+  const wrapParams = isWrapped(params) ? params : value(params);
+
+  // create empty return values
+  const isLoading = value();
+  const error = value();
+  const data = value();
+
+  // abort controller
   let controller = null;
 
-  onCreated(async () => {
+  function abort() {
+    isLoading.value = false;
+    error.value = undefined;
+    if (controller !== null) {
+      controller.abort();
+    }
+  }
+
+  function retry() {
+    // unwrap the original promise as it is optionally wrapped
+    const origPromiseFn = isWrapped(promiseFn) ? promiseFn.value : promiseFn;
+    // create a new promise and trigger watch
+    wrapPromiseFn.value = async (params, signal) =>
+      origPromiseFn(params, signal);
+  }
+
+  // watch for change in arguments, which triggers immediately initially
+  watch([wrapPromiseFn, wrapParams], async ([newPromiseFn, newParams]) => {
     try {
+      abort();
+      isLoading.value = true;
       controller = new AbortController();
       const { signal } = controller;
-      const result = await promiseFn(params, signal);
+      const result = await newPromiseFn(newParams, signal);
       data.value = result;
     } catch (e) {
       error.value = e;
@@ -30,26 +65,26 @@ export function useAsync(promiseFn, params) {
   });
 
   onBeforeDestroy(() => {
-    if (controller !== null) {
-      controller.abort();
-    }
+    abort();
   });
 
   return {
     isLoading,
     error,
-    data
+    data,
+    abort,
+    retry
   };
 }
 
 /**
- * Fetch helper function that accepts the same arguments as `fetch` and returns the same reactive values as `useAsync`.
+ * Fetch helper function that accepts the same arguments as `fetch` and returns the same values as `useAsync`.
  * If the `Accept` header is set to `application/json` in the `requestInit` object, the response will be parsed as JSON,
  * else text.
  *
- * @param {string|object} requestInfo URL or request object.
- * @param {object} requestInit Init parameters for the request.
- * @returns {object} Object literal containing `isLoading`, `error` and `data` value wrappers.
+ * @param {string|object|ValueWrapper} requestInfo (optionally wrapped) URL or request object.
+ * @param {object|ValueWrapper} requestInit (optionally wrapped) init parameters for the request.
+ * @returns {object} Object literal containing same return values as `useAsync`.
  */
 export function useFetch(requestInfo, requestInit = {}) {
   async function doFetch(params, signal) {
